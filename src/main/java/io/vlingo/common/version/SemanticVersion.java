f415a02492355e1fcb014ac31da646d9d1b3d84e
Vaughn Vernon <vaughn@forcomprehension.com>
2018-11-17 10:29:17 +0100

    Enhanced semantic versioning.

--- src/main/java/io/vlingo/common/version/SemanticVersion.java
+++ src/main/java/io/vlingo/common/version/SemanticVersion.java
@@ -17,6 +17,18 @@ public class SemanticVersion {
   public static final int PATCH_MASK = 0x000000ff;
   public static final int PATCH_MAX = 255;
 
+  public static SemanticVersion from(final int major, final int minor, final int patch) {
+    return new SemanticVersion(major, minor, patch);
+  }
+
+  public static SemanticVersion from(final String version) {
+    final String[] parts = version.split("\\.");
+    final int major = Integer.parseInt(parts[0]);
+    final int minor = Integer.parseInt(parts[1]);
+    final int patch = Integer.parseInt(parts[2]);
+    return new SemanticVersion(major, minor, patch);
+  }
+
   public static String toString(final int version) {
     return "" + (version >> MAJOR_SHIFT) + "." + ((version & MINOR_MASK) >> MINOR_SHIFT) + "." + (version & PATCH_MASK);
   }
@@ -32,5 +44,56 @@ public class SemanticVersion {
       throw new IllegalArgumentException("Patch version must be 0 to " + PATCH_MAX);
     }
     return ((major << MAJOR_SHIFT) & MAJOR_MASK) | ((minor << MINOR_SHIFT) & MINOR_MASK) | (patch & PATCH_MASK);
+  }
+
+  public static int toValue(final String version) {
+    final String[] parts = version.split("\\.");
+    return toValue(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]), Integer.parseInt(parts[2]));
+  }
+
+  public final int major;
+  public final int minor;
+  public final int patch;
+
+  public boolean isCompatibleWith(final SemanticVersion previous) {
+    if (major == previous.major && minor == previous.minor && patch == previous.patch + 1) {
+      return true;
+    }
+    if (major == previous.major && minor == previous.minor + 1 && patch == previous.patch) {
+      return true;
+    }
+    if (major == previous.major + 1 && minor == previous.minor && patch == previous.patch) {
+      return true;
+    }
+    return false;
+  }
+
+  public SemanticVersion withIncrementedMajor() {
+    return new SemanticVersion(major + 1, minor, patch);
+  }
+
+  public SemanticVersion withIncrementedMinor() {
+    return new SemanticVersion(major, minor + 1, patch);
+  }
+
+  public SemanticVersion withIncrementedPatch() {
+    return new SemanticVersion(major, minor, patch + 1);
+  }
+
+  public String toString() {
+    return "" + major + "." + minor + "." + patch;
+  }
+
+  public int toValue() {
+    return toValue(major, minor, patch);
+  }
+
+  private SemanticVersion(final int major, final int minor, final int patch) {
+    assert (major >= 0 && major <= MAJOR_MAX);
+    this.major = major;
+    assert (minor >= 0 && minor <= MINOR_MAX);
+    this.minor = minor;
+    assert (patch >= 0 && patch <= PATCH_MAX);
+    this.patch = patch;
   }
 }
--- src/test/java/io/vlingo/common/version/SemanticVersionTest.java
+++ src/test/java/io/vlingo/common/version/SemanticVersionTest.java
@@ -8,6 +8,8 @@
 package io.vlingo.common.version;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -47,6 +49,63 @@ public class SemanticVersionTest {
 
     final int semanticVersion_32767_255_255 = SemanticVersion.toValue(32767, 255, 255);
     assertEquals("32767.255.255", SemanticVersion.toString(semanticVersion_32767_255_255));
+  }
+
+  @Test
+  public void testThatStringToValueParses() {
+    final int semanticVersion_1_0_0 = SemanticVersion.toValue(1, 0, 0);
+    final int semanticVersion_1_0_0_again = SemanticVersion.toValue(SemanticVersion.toString(semanticVersion_1_0_0));
+    assertEquals(semanticVersion_1_0_0, semanticVersion_1_0_0_again);
+
+    final int semanticVersion_0_1_0 = SemanticVersion.toValue(0, 1, 0);
+    final int semanticVersion_0_1_0_again = SemanticVersion.toValue(SemanticVersion.toString(semanticVersion_0_1_0));
+    assertEquals(semanticVersion_0_1_0, semanticVersion_0_1_0_again);
+
+    final int semanticVersion_0_0_1 = SemanticVersion.toValue(0, 0, 1);
+    final int semanticVersion_0_0_1_again = SemanticVersion.toValue(SemanticVersion.toString(semanticVersion_0_0_1));
+    assertEquals(semanticVersion_0_0_1, semanticVersion_0_0_1_again);
+
+    final int semanticVersion_1_1_0 = SemanticVersion.toValue(1, 1, 0);
+    final int semanticVersion_1_1_0_again = SemanticVersion.toValue(SemanticVersion.toString(semanticVersion_1_1_0));
+    assertEquals(semanticVersion_1_1_0, semanticVersion_1_1_0_again);
+
+    final int semanticVersion_1_1_1 = SemanticVersion.toValue(1, 1, 1);
+    final int semanticVersion_1_1_1_again = SemanticVersion.toValue(SemanticVersion.toString(semanticVersion_1_1_1));
+    assertEquals(semanticVersion_1_1_1, semanticVersion_1_1_1_again);
+  }
+
+  @Test
+  public void testVersionCompatibility() {
+    final SemanticVersion version = SemanticVersion.from(1, 0, 0);
+
+    final SemanticVersion majorBump = SemanticVersion.from(2, 0, 0);
+    assertTrue(majorBump.isCompatibleWith(version));
+
+    final SemanticVersion minorBump = SemanticVersion.from(1, 1, 0);
+    assertTrue(minorBump.isCompatibleWith(version));
+
+    final SemanticVersion patchBump = SemanticVersion.from(1, 0, 1);
+    assertTrue(patchBump.isCompatibleWith(version));
+  }
+
+  @Test
+  public void testVersionIncompatibility() {
+    final SemanticVersion version = SemanticVersion.from(1, 0, 0);
+
+    final SemanticVersion majorTooHigh = SemanticVersion.from(3, 0, 0);
+    assertFalse(majorTooHigh.isCompatibleWith(version));
+
+    final SemanticVersion majorBumpMinorTooHigh = SemanticVersion.from(2, 1, 0);
+    assertFalse(majorBumpMinorTooHigh.isCompatibleWith(version));
+
+    final SemanticVersion minorTooHigh = SemanticVersion.from(1, 2, 0);
+    assertFalse(minorTooHigh.isCompatibleWith(version));
+
+    final SemanticVersion minorBumpPatchTooHigh = SemanticVersion.from(1, 1, 1);
+    assertFalse(minorBumpPatchTooHigh.isCompatibleWith(version));
+
+    final SemanticVersion patchTooHigh = SemanticVersion.from(1, 0, 2);
+    assertFalse(patchTooHigh.isCompatibleWith(version));
   }
 
   @Test(expected=IllegalArgumentException.class)
