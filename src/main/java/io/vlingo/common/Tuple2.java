111216aa19bc9ed869452faf196a8e8998cbdf61
VaughnVernon <vaughn@forcomprehension.com>
2019-09-11 22:51:40 -0700

    Implemented tuple(), forEach(), and map().

--- src/main/java/io/vlingo/common/Tuple2.java
+++ src/main/java/io/vlingo/common/Tuple2.java
@@ -7,12 +7,38 @@
 
 package io.vlingo.common;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
 public class Tuple2<A, B> {
   public final A _1;
   public final B _2;
-  
+
   public static <A, B> Tuple2<A, B> from(final A a, final B b) {
     return new Tuple2<A, B>(a, b);
+  }
+
+  public static <A, B> Tuple2<A, B> tuple(final A a, final B b) {
+    return new Tuple2<A, B>(a, b);
+  }
+
+  @SuppressWarnings("unchecked")
+  <T> void forEach(final Consumer<? super T> consumer) {
+    consumer.accept((T) _1);
+    consumer.accept((T) _2);
+  }
+
+  @SuppressWarnings("unchecked")
+  <TA,TB> Collection<TB> map(final Function<? super TA, ? super TB> function) {
+    final List<TB> list = new ArrayList<>(2);
+
+    list.add((TB) function.apply((TA) _1));
+    list.add((TB) function.apply((TA) _2));
+
+    return list;
   }
 
   private Tuple2(final A a, final B b) {
