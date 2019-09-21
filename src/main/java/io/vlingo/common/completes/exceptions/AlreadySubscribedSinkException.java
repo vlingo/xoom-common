82de5277bf9dd2545a710ac3673ebdaf07fb9660
Kevin Mas Ruiz <masruizkevin@gmail.com>
2019-09-21 22:40:48 +0200

    Add copyright notice

--- src/main/java/io/vlingo/common/completes/exceptions/AlreadySubscribedSinkException.java
+++
@@ -1,7 +0,0 @@
-package io.vlingo.common.completes.exceptions;
-
-public class AlreadySubscribedSinkException extends RuntimeException {
-    public AlreadySubscribedSinkException() {
-        super("Sinks can only receive one subscriber. Please check your code for multiple subscriptions to a chain.");
-    }
-}
