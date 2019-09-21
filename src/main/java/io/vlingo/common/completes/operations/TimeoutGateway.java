57b28f774ab883878e5fb8f85e1d37582ac12a6b
Kevin Mas Ruiz <masruizkevin@gmail.com>
2019-09-21 20:11:24 +0200

    Decouple timeouts from operations

---
+++ src/main/java/io/vlingo/common/completes/operations/TimeoutGateway.java
@@ -0,0 +1,43 @@
+package io.vlingo.common.completes.operations;
+
+import io.vlingo.common.Cancellable;
+import io.vlingo.common.Scheduled;
+import io.vlingo.common.Scheduler;
+import io.vlingo.common.completes.Operation;
+
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class TimeoutGateway<Receives> extends Operation<Receives, Receives> implements Scheduled<Void> {
+    private final Scheduler scheduler;
+    private final long timeout;
+    private Cancellable cancellable;
+    private AtomicBoolean didTimeout;
+
+    public TimeoutGateway(Scheduler scheduler, long timeout) {
+        this.scheduler = scheduler;
+        this.timeout = timeout;
+        this.didTimeout = new AtomicBoolean(false);
+        startTimer();
+    }
+
+    @Override
+    public void onOutcome(Receives receives) {
+        if (!didTimeout.get()) {
+            this.cancellable.cancel();
+            emitOutcome(receives);
+            startTimer();
+        }
+    }
+
+    @Override
+    public void intervalSignal(Scheduled<Void> scheduled, Void data) {
+        emitError(new TimeoutException());
+        emitCompletion();
+        didTimeout.set(true);
+    }
+
+    private void startTimer() {
+        this.cancellable = scheduler.scheduleOnce(this, null, 0, timeout);
+    }
+}
