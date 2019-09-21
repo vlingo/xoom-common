ea98bdd07f0ceb2a3c99f3798d99233c04e36539
Kevin Mas Ruiz <masruizkevin@gmail.com>
2019-09-21 19:12:42 +0200

    Tested error handling

--- src/test/java/io/vlingo/common/completes/operations/AndThenToSourceTest.java
+++ src/test/java/io/vlingo/common/completes/operations/AndThenToSourceTest.java
@@ -43,4 +43,30 @@ public class AndThenToSourceTest {
         source.subscribe(operation);
         source.flush();
     }
+
+    @Test
+    public void shouldEmitAnyErrorReceivedOnTheSourceStream() {
+        verifier.outcomeIs(2);
+        verifier.outcomeIs(4);
+        verifier.failedWith(NullPointerException.class);
+
+        source.emitOutcome(2);
+        source.emitOutcome(null);
+        source.emitCompletion();
+
+        AndThenToSource<Integer, Integer> operation = new AndThenToSource<>(v -> {
+            TestSource<Integer> multiplier = new TestSource<>();
+            multiplier.emitOutcome(v);
+            multiplier.emitOutcome(v * 2);
+            multiplier.emitCompletion();
+            multiplier.flush();
+
+            return multiplier;
+        });
+
+        operation.subscribe(verifier.asSink());
+
+        source.subscribe(operation);
+        source.flush();
+    }
 }
