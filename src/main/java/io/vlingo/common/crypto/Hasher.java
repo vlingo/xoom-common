422dcadad98bda8f8b1bfb94199f86fe10901e75
VaughnVernon <vaughn@forcomprehension.com>
2019-10-26 11:20:58 -0700

    Use properties parameter.

--- src/main/java/io/vlingo/common/crypto/Hasher.java
+++ src/main/java/io/vlingo/common/crypto/Hasher.java
@@ -13,14 +13,14 @@ public interface Hasher {
   static Hasher defaultHasher(final Properties properties) {
     switch (properties.getProperty("crypto.type", "(unknown)")) {
     case "argon2":
-      final int cryptoArgon2MaxDuration = Integer.parseInt(System.getProperties().getProperty("crypto.argon2.max.duration", "1000"));
-      final int cryptoArgon2MemoryCost = Integer.parseInt(System.getProperties().getProperty("crypto.argon2.memory.cost", "65536"));
-      final int cryptoArgon2Parallelism = Integer.parseInt(System.getProperties().getProperty("crypto.argon2.parallelism", "1"));
+      final int cryptoArgon2MaxDuration = Integer.parseInt(properties.getProperty("crypto.argon2.max.duration", "10"));
+      final int cryptoArgon2MemoryCost = Integer.parseInt(properties.getProperty("crypto.argon2.memory.cost", "65536"));
+      final int cryptoArgon2Parallelism = Integer.parseInt(properties.getProperty("crypto.argon2.parallelism", "1"));
       return new Argon2Hasher(cryptoArgon2MaxDuration, cryptoArgon2MemoryCost, cryptoArgon2Parallelism);
     case "scrypt":
-      final int cryptoScrypt_N_costFactor = Integer.parseInt(System.getProperties().getProperty("crypto.scrypt.N.cost.factor", "16384"));
-      final int cryptoScrypt_r_Blocksize = Integer.parseInt(System.getProperties().getProperty("crypto.scrypt.r.blocksize", "8"));
-      final int cryptoScrypt_p_parallelization = Integer.parseInt(System.getProperties().getProperty("crypto.scrypt.p.parallelization", "1"));
+      final int cryptoScrypt_N_costFactor = Integer.parseInt(properties.getProperty("crypto.scrypt.N.cost.factor", "16384"));
+      final int cryptoScrypt_r_Blocksize = Integer.parseInt(properties.getProperty("crypto.scrypt.r.blocksize", "8"));
+      final int cryptoScrypt_p_parallelization = Integer.parseInt(properties.getProperty("crypto.scrypt.p.parallelization", "1"));
       return new SCryptHasher(cryptoScrypt_N_costFactor, cryptoScrypt_r_Blocksize, cryptoScrypt_p_parallelization);
     case "bcrypt":
       return new BCryptHasher();
