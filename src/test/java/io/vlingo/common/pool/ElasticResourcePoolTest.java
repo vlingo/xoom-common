284ed74b8e79aaff4da9d379f919b9276b2c2b2f
Alexandros Bourantas <buritos@gmail.com>
2019-11-06 20:33:51 +0100

    fixes ElasticResourcePoolTest

--- src/test/java/io/vlingo/common/pool/ElasticResourcePoolTest.java
+++ src/test/java/io/vlingo/common/pool/ElasticResourcePoolTest.java
@@ -3,6 +3,7 @@ package io.vlingo.common.pool;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ElasticResourcePoolTest extends ResourcePoolTest {
 
@@ -13,24 +14,37 @@ public class ElasticResourcePoolTest extends ResourcePoolTest {
   private final ResourcePool<Integer, Void> pool =
       new ElasticResourcePool<>(minIdle, new TestResourceFactory());
 
-
   @Test
   public void testInitialState() {
-    assertEquals(minIdle, pool.size());
+    assertEquals("the pool did not start with the desired number of resources",
+        minIdle, pool.size());
+
     ResourcePoolStats stats = pool.stats();
-    assertEquals(pool.size(), stats.idle);
-    assertEquals(minIdle, stats.allocations);
-    assertEquals(0, stats.evictions);
-    assertEquals(0, stats.inUse);
+    assertEquals("the number of idle resources in stats did not match the size of the pool",
+        pool.size(), stats.idle);
+    assertEquals("the number of allocations in stats did not match the desired number of initial resources",
+        minIdle, stats.allocations);
+    assertEquals("the number of evictions in stats did not start at 0",
+        0, stats.evictions);
+    assertEquals("the number of inUse in stats did not start at 0",
+        0, stats.inUse);
   }
 
   @Test
   public void testConcurrent() {
-    testConcurrent(pool, threads, clients);
+    int maxIdle = testConcurrent(pool, threads, clients);
     ResourcePoolStats stats = pool.stats();
-    assertEquals(stats.allocations - stats.idle, stats.evictions);
-    assertEquals(threads - minIdle, stats.misses);
-    assertEquals(stats.misses, stats.evictions);
-    assertEquals(0, stats.inUse);
+    assertEquals("the number of idle in stats is not equal to the pool size",
+        stats.idle, pool.size());
+    assertEquals("the number of inUse in stats is not equal to 0",
+        0, stats.inUse);
+    assertTrue("the pool did not scale up to accommodate more threads than the initial resources",
+        maxIdle > minIdle);
+    assertTrue("the pool did not scale down to release memory as threads exited",
+        maxIdle > stats.idle);
+    assertTrue("the pool shrank below the desired minimum number of resources",
+        stats.idle >= minIdle);
+    assertTrue("the pool didn't count evictions",
+        0 < stats.evictions);
   }
 }
--- src/test/java/io/vlingo/common/pool/ResourcePoolTest.java
+++ src/test/java/io/vlingo/common/pool/ResourcePoolTest.java
@@ -1,21 +1,20 @@
 package io.vlingo.common.pool;
 
+import java.util.List;
 import java.util.concurrent.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
-import static org.junit.Assert.assertEquals;
 
 abstract class ResourcePoolTest {
 
   private static ThreadLocalRandom Random = ThreadLocalRandom.current();
 
-  static <R, A> void testConcurrent(ResourcePool<R, A> pool, int threads, int clients) {
+  static <R, A> int testConcurrent(ResourcePool<R, A> pool, int threads, int clients) {
     final ExecutorService exec = Executors.newFixedThreadPool(threads);
-    CompletionService<Void> completionService =
+    CompletionService<Integer> completionService =
         new ExecutorCompletionService<>(exec);
 
-    Callable<Void> call = () -> {
+    Callable<Integer> call = () -> {
       R resource = null;
       try {
         do {
@@ -26,23 +25,25 @@ abstract class ResourcePoolTest {
         e.printStackTrace();
       } finally {
         pool.release(resource);
-        ResourcePoolStats stats = pool.stats();
-        assertEquals(stats.allocations, stats.evictions + stats.idle + stats.inUse);
       }
-      return null;
+
+      return pool.stats().idle;
     };
 
-    IntStream.range(0, clients)
-        .mapToObj((int i) -> completionService.submit(call))
-        .collect(Collectors.toList())
-        .forEach((f) -> {
-          try {
-            completionService.take().get();
-          } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-          }
-        });
+    int maxIdle = 0;
 
-    assertEquals(pool.stats().idle, pool.size());
+    List<Future<Integer>> futures = IntStream.range(0, clients)
+        .mapToObj((int i) -> completionService.submit(call))
+        .collect(Collectors.toList());
+
+    for (Future<Integer> f : futures) {
+      try {
+        maxIdle = Math.max(maxIdle, completionService.take().get());
+      } catch (InterruptedException | ExecutionException e) {
+        e.printStackTrace();
+      }
+    }
+
+    return maxIdle;
   }
 }
