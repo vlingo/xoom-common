8d0aa75064790089c9a7a9ae68d1915ac3e56c22
Hamza JGUERIM <hamzajg@gmail.com>
2021-05-11 04:25:16 +0100

    Fixing Indentation..

--- src/main/java/io/vlingo/xoom/common/implnative/NativeImpl.java
+++ src/main/java/io/vlingo/xoom/common/implnative/NativeImpl.java
@@ -10,42 +10,42 @@ import org.graalvm.nativeimage.c.type.CTypeConversion;
 import java.util.Date;
 
 public final class NativeImpl {
-    @CEntryPoint(name = "Java_io_vlingo_xoom_commonnative_Native_from")
-    public static int from(@CEntryPoint.IsolateThreadContext long isolateId, CCharPointer message) {
-        final String messageString = CTypeConversion.toJavaString(message);
-        MessageExchangeReader.from(new Message() {
+  @CEntryPoint(name = "Java_io_vlingo_xoom_commonnative_Native_from")
+  public static int from(@CEntryPoint.IsolateThreadContext long isolateId, CCharPointer message) {
+    final String messageString = CTypeConversion.toJavaString(message);
+    MessageExchangeReader.from(new Message() {
 
-            @Override
-            public String id() {
-                // TODO Auto-generated method stub
-                return new IdentityGenerator.NameBasedIdentityGenerator().generate().toString();
-            }
+      @Override
+      public String id() {
+        // TODO Auto-generated method stub
+        return new IdentityGenerator.NameBasedIdentityGenerator().generate().toString();
+      }
 
-            @Override
-            public Date occurredOn() {
-                // TODO Auto-generated method stub
-                return null;
-            }
+      @Override
+      public Date occurredOn() {
+        // TODO Auto-generated method stub
+        return null;
+      }
 
-            @Override
-            public String payload() {
-                // TODO Auto-generated method stub
-                return messageString;
-            }
+      @Override
+      public String payload() {
+        // TODO Auto-generated method stub
+        return messageString;
+      }
 
-            @Override
-            public String type() {
-                // TODO Auto-generated method stub
-                return null;
-            }
+      @Override
+      public String type() {
+        // TODO Auto-generated method stub
+        return null;
+      }
 
-            @Override
-            public String version() {
-                // TODO Auto-generated method stub
-                return null;
-            }
+      @Override
+      public String version() {
+        // TODO Auto-generated method stub
+        return null;
+      }
 
-        });
-        return 0;
-    }
+    });
+    return 0;
+  }
 }
