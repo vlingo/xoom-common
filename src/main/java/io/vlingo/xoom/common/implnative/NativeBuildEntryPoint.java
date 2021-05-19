26374f3380b9ddfe15e3188fff9b39b4aa0c7775
Hamza JGUERIM <hamzajg@gmail.com>
2021-05-19 19:57:03 +0100

    Refactoring..

--- src/main/java/io/vlingo/xoom/common/implnative/NativeBuildEntryPoint.java
+++ src/main/java/io/vlingo/xoom/common/implnative/NativeBuildEntryPoint.java
@@ -9,7 +9,7 @@ import org.graalvm.nativeimage.c.type.CTypeConversion;
 
 import java.util.Date;
 
-public final class NativeImpl {
+public final class NativeBuildEntryPoint {
   @CEntryPoint(name = "Java_io_vlingo_xoom_commonnative_Native_from")
   public static int from(@CEntryPoint.IsolateThreadContext long isolateId, CCharPointer message) {
     final String messageString = CTypeConversion.toJavaString(message);
